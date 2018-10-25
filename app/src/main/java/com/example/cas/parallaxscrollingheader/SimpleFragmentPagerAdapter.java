898311f4259822390bfdd472a3a0e2a4208dce2c
Vikas Pal <pal.vikas210@gmail.com>
2018-10-25 12:51:46 +0530

    Addded SimpleFragmentPagerAdapter

---
+++ app/src/main/java/com/example/cas/parallaxscrollingheader/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,28 @@
+package com.example.cas.parallaxscrollingheader;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    public SimpleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position==0){
+            return new FirstFragment();
+        }else if (position==1){
+            return new SecondFragment();
+        }else{
+            return new ThirdFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
