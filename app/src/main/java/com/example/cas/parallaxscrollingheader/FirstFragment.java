d9f7135d34594acd27eeff6bfc514c85e1375722
Vikas Pal <pal.vikas210@gmail.com>
2018-10-25 13:04:01 +0530

    Set adapter to  videw pager in MainActivity

--- app/src/main/java/com/example/cas/parallaxscrollingheader/FirstFragment.java
+++ app/src/main/java/com/example/cas/parallaxscrollingheader/FirstFragment.java
@@ -30,19 +30,8 @@ public class FirstFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-     View view=inflater.inflate(R.layout.fragment_first,container,false);
-
-     final FrameLayout frameLayout=(FrameLayout)view.findViewById(R.id.firstfrag_bg);
-     frameLayout.setBackgroundColor(color);
-
-        RecyclerView recyclerView=(RecyclerView)view.findViewById(R.id.firstfrag_scrollableview);
-
-        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getActivity().getBaseContext());
-        recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerView.setHasFixedSize(true);
-
-
-        return view;
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
     }
 
 }
